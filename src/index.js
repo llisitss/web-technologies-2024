2cfa9c0a0f535c4bef5db0a5b50f028e7b047a52
oleg <r7rrrrr7@gmail.com>
2025-12-17 22:35:24 +0500

    lesson5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log(document.getElementById('test-id'))
-// })
+const menuButtons = document.querySelectorAll('.open-menu-button, .close-menu-button');
+const toggleElements = document.querySelectorAll('.open-menu-button, .close-menu-button, .menu, .header-address, .mail');
+const title = document.querySelector('.title');
+const header = document.querySelector('.header');
+
+const toggleMenu = () => {
+    toggleElements.forEach(element => {
+        element.classList.toggle('hidden');
+    });
+    
+    title.classList.toggle('menu-title');
+    header.classList.toggle('show-menu');
+};
+
+menuButtons.forEach(button => {
+    button.addEventListener('click', toggleMenu);
+});
